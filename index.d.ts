dcacc0d70f4136c85e4b9fce34f42ef5838aeb1e
Marcel Beumer <marcel@marcelbeumer.com>
2020-02-29 00:10:01 +0100

    Add test fn variations to TS defs

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
+export type SyncTestFn = () => void;
+export type CallbackTestFn = (done: () => void) => void;
+export type AsyncTestFn = () => Promise<any>;
 export interface Test {
-  (description: string, fn: Function): void
-  only: Test
-  skip: Test
+  (description: string, fn: SyncTestFn | CallbackTestFn | AsyncTestFn): void;
+  only: Test;
+  skip: Test;
 }
 declare const test: Test;
 export default test;
